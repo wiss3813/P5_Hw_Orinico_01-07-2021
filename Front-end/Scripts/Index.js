6eb5f21f906c0f2934f23af79ac51442d1010592
Wissem <wiss3813@gmail.com>
2021-10-16 22:38:42 +0200

    Index.js ADD

--- Front-end/Scripts/Index.js
+++ Front-end/Scripts/Index.js
@@ -7,15 +7,8 @@ function main() {
 // Récupérer les articles depuis l'API
 function getArticles() {
   fetch("http://localhost:8888/api/teddies")
-    .then(function (res) {
-      return res.json();
-    })
-    .catch((error) => {
-      let productsContainer = document.querySelector(".products-container");
-      productsContainer.innerHTML =
-        "Nous n'avons pas réussi à afficher nos nounours. Avez vous bien lancé le serveur local (Port 3000) ? <br>Si le problème persiste, contactez-nous.";
-      productsContainer.style.textAlign = "center";
-      productsContainer.style.padding = "30vh 0";
+    .then(function (response) {
+      return response.json();
     })
 
     // Dispatcher les données de chaque produit (prix, nom...) dans le DOM
@@ -62,7 +55,3 @@ function getArticles() {
       }
     });
 }
-
-
-
-
